21344d07af70fa46f59043a5f881b47ff08b207b
dhruvkelawala <dhruvrk2000@gmail.com>
2021-12-27 17:39:54 +0530

    Update normalizeAccount

--- src/core/normalizers.ts
+++ src/core/normalizers.ts
@@ -36,35 +36,35 @@ export function normalizeAccount(_address: string): string {
 
   const address =
     _address.substring(0, 2) === '0x' ? _address : `0x${_address}`;
-  const chars = address
-    .toLowerCase()
-    .substring(2)
-    .split('');
+  //   const chars = address
+  //     .toLowerCase()
+  //     .substring(2)
+  //     .split('');
 
-  const charsArray = new Uint8Array(63);
-  for (let i = 0; i < 63; i++) {
-    charsArray[i] = chars[i].charCodeAt(0);
-  }
-  const hashed = arrayify(keccak256(charsArray));
+  //   const charsArray = new Uint8Array(63);
+  //   for (let i = 0; i < 63; i++) {
+  //     charsArray[i] = chars[i].charCodeAt(0);
+  //   }
+  //   const hashed = arrayify(keccak256(charsArray));
 
-  for (let i = 0; i < 63; i += 2) {
-    if (hashed[i >> 1] >> 4 >= 8) {
-      chars[i] = chars[i].toUpperCase();
-    }
-    if ((hashed[i >> 1] & 0x0f) >= 8) {
-      chars[i + 1] = chars[i + 1].toUpperCase();
-    }
-  }
+  //   for (let i = 0; i < 63; i += 2) {
+  //     if (hashed[i >> 1] >> 4 >= 8) {
+  //       chars[i] = chars[i].toUpperCase();
+  //     }
+  //     if ((hashed[i >> 1] & 0x0f) >= 8) {
+  //       chars[i + 1] = chars[i + 1].toUpperCase();
+  //     }
+  //   }
 
-  const addressChecksum = `0x${chars.join('')}`;
+  //   const addressChecksum = `0x${chars.join('')}`;
 
-  invariant(
-    !(
-      address.match(/([A-F].*[a-f])|([a-f].*[A-F])/) &&
-      address !== addressChecksum
-    ),
-    `Bad address checksum ${address} ${addressChecksum}`
-  );
+  //   invariant(
+  //     !(
+  //       address.match(/([A-F].*[a-f])|([a-f].*[A-F])/) &&
+  //       address !== addressChecksum
+  //     ),
+  //     `Bad address checksum ${address} ${addressChecksum}`
+  //   );
 
-  return addressChecksum;
+  return address;
 }
